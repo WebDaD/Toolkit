bdb56e31d8ff5cccd5fcaae26fc2ea86d3c1cfa4
DSigmund <sigmund.dominik@googlemail.com>
2014-07-28 15:12:13 +0200

    #59 Added class orderby

---
+++ WebDaD.Toolkit/Database/OrderBy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebDaD.Toolkit.Database
+{
+    public class OrderBy
+    {
+        public readonly static int ASC = 1;
+        public readonly static int DESC = 0;
+
+        private string field;
+
+        private int direction;
+
+        public OrderBy(string field, int direction)
+        {
+            this.field = field;
+            this.direction = direction;
+        }
+
+        public override string ToString()
+        {
+            return "ORDER BY `"+this.field+"` "+((this.direction==ASC)?"ASC":"DESC");
+        }
+    }
+}
