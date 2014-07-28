5f50f2a435a9d14ccf3f3d86a1bee07849613125
DSigmund <sigmund.dominik@googlemail.com>
2014-07-28 15:13:21 +0200

    #60 Added class groupby

---
+++ WebDaD.Toolkit/Database/GroupBy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebDaD.Toolkit.Database
+{
+    public class GroupBy
+    {
+
+        private string field;
+
+
+        public GroupBy(string field)
+        {
+            this.field = field;
+        }
+
+        public override string ToString()
+        {
+            return "GROUP BY `"+this.field+"`";
+        }
+
+    }
+}
