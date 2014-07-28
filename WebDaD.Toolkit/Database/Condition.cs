3529c46da1313de4735ec4fc538109ee50db722e
DSigmund <sigmund.dominik@googlemail.com>
2014-07-28 15:08:34 +0200

    # 58 New Class Condition

---
+++ WebDaD.Toolkit/Database/Condition.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebDaD.Toolkit.Database
+{
+    public class Condition
+    {
+        private string field;
+
+        private ConditionOperator coperator;
+
+        private string value;
+
+        public Condition(string field, ConditionOperator co, string value)
+        {
+            this.field = field;
+            this.coperator = co;
+            this.value = value;
+        }
+        public override string ToString()
+        {
+            string r = "`"+this.field+"`";
+            switch (this.coperator)
+            {
+                case ConditionOperator.IS:
+                    r += "=";
+                    break;
+                case ConditionOperator.ISNOT:
+                    r += "!=";
+                    break;
+                case ConditionOperator.LESSTHAN:
+                    r += "<";
+                    break;
+                case ConditionOperator.LESSOREQUALTHAN:
+                    r += "<=";
+                    break;
+                case ConditionOperator.GREATERTHAN:
+                    r += ">";
+                    break;
+                case ConditionOperator.GREATEROREQUALTHAN:
+                    r += ">=";
+                    break;
+                case ConditionOperator.LIKE:
+                    r += " LIKE ";
+                    break;
+                default:
+                    break;
+            }
+            if (this.coperator != ConditionOperator.LIKE)
+            {
+                r += "'" + value + "'";
+            }
+            else
+            {
+                r += "'%" + value + "%'";
+            }
+            return r;
+        }
+    }
+}
---
+++ WebDaD.Toolkit/Database/ConditionOperator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebDaD.Toolkit.Database
+{
+    public enum ConditionOperator
+    {
+        IS,
+        ISNOT,
+        LESSTHAN,
+        LESSOREQUALTHAN,
+        GREATERTHAN,
+        GREATEROREQUALTHAN,
+        LIKE
+    }
+}
