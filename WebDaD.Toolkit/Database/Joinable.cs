1320885857cbb8424df57cf4a242ad0cab9b3277
DSigmund <sigmund.dominik@googlemail.com>
2014-07-28 15:02:47 +0200

    #61 Created Interface Joinable

---
+++ WebDaD.Toolkit/Database/Joinable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebDaD.Toolkit.Database
+{
+    public interface Joinable
+    {
+        string JoinOn(Joinable jointable);
+        string GetTableName();
+    }
+}
