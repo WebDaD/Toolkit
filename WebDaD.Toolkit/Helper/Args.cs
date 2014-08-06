67005cbe739cb9da9b4879669c193da3bb28b9c6
DSigmund <sigmund.dominik@googlemail.com>
2014-08-06 10:13:08 +0200

    #10 Added Args.cs (no functions yet)

---
+++ WebDaD.Toolkit/Helper/Args.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebDaD.Toolkit.Helper
+{
+    public class Args
+    {
+        private Dictionary<string, string> arg_value;
+
+        private bool silent;
+        private bool autostart;
+        private WebDaD.Toolkit.Database.Database db;
+        
+
+        public Args(string[] args)
+        {
+            arg_value = new Dictionary<string, string>();
+            //TODO: Parse args, fill dictionary, fill default atrs
+        }
+    }
+}
