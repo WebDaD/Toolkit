498cde44ab0993f7a694791b33c42ef4ebd5d9cb
DSigmund <sigmund.dominik@googlemail.com>
2014-08-06 14:06:52 +0200

    #10 Added ComboBoxItem

---
+++ WebDaD.Toolkit/Helper/ComboBoxItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebDaD.Toolkit.Helper
+{
+    public class ComboBoxItem
+    {
+        private string value;
+        private string text;
+        public string Value { get { return value; }}
+        public string Text { get { return text; }}
+
+        public ComboBoxItem(string value, string text)
+        {
+            this.value = value;
+            this.text = text;
+        }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
